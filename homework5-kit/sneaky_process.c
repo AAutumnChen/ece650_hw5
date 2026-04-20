554ae3636e78e1032f3923b6805d113f0ec3c169
AAutumnChen <camo.chen66@gmail.com>
2026-04-20 16:33:06 +0800

    checklist feat-3

--- homework5-kit/sneaky_process.c
+++ homework5-kit/sneaky_process.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-  puts("sneaky_process skeleton");
+  int ch;
+
+  printf("sneaky_process pid = %d\n", getpid());
+
+  while ((ch = getchar()) != EOF) {
+    if (ch == 'q') {
+      break;
+    }
+  }
+
   return 0;
 }
